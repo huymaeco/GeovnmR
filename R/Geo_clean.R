753d4725b7813218cecaa94121d4c782a9757f24
huymaeco <levuhuy280702@gmail.com>
2024-06-26 11:52:26 +0700

    add some function

--- R/Geo_clean.R
+++ R/Geo_clean.R
@@ -1,15 +1,14 @@
-#' @title Geo_extract
+#' @title Geo_clean
 #'
-#' @description Provides a CSV data of any geo data for every coumtry in the world.
+#' @description Provides a CSV data with your own Geo data (That dowload from somewhere)
 #'
 #' @param ISO ISO3 code of your targeted country
 #' @param Level Level of your targeted ADM shape file
-#' @param dataset A string representing the dataset to be used (e.g., "cru-ts").
-#' @param timestep Time resolution for the data (e.g., "yearly", "monthly").
+#' @param dataset dataset by your own with NetCDF format
 #'
 #' @return A data frame/csv object that contains geodata for the specified time period.
 #' @examples
-#' data <- Geo_extract(ISO = "VNM", Level = "2", dataset = "cru-ts", timestep = "yearly")
+#' data <- Geo_extract(ISO = "VNM", Level = "1", dataset = "Your nc path file")
 #' @export
 #' @importFrom dplyr %>%
 #' @importFrom geodata gadm
@@ -17,7 +16,7 @@
 #' @importFrom terra rast crop extract
 #' @importFrom sf st_as_sf
 
-Geo_extract <- function(ISO, Level, dataset, timestep) {
+Geo_clean <- function(ISO, Level, dataset) {
   main_path <- getwd()
   # ADM level 2 ----
   border_sf <- geodata::gadm(
@@ -26,18 +25,10 @@ Geo_extract <- function(ISO, Level, dataset, timestep) {
     path = main_path
   ) |>
     sf::st_as_sf()
-  # Extract data in netcdf format----
-  ## Download data ----
-  pRecipe::download_data(
-    dataset = dataset,
-    path = main_path,
-    domain = "raw",
-    timestep = timestep
-  )
+  # Load your data in netcdf format----
   ## Load the downloaded .nc file (assuming there's only one for simplicity)----
-  nc_files <- list.files(pattern = "\\.nc$")
-  if (length(nc_files) == 0) stop("No .nc files found in the directory.")
-  main_data <- terra::rast(nc_files[1]) |>
+
+  main_data <- terra::rast(dataset) |>
     terra::crop(border_sf)
 
   # Clean data and convert to CSV ----
@@ -48,11 +39,9 @@ Geo_extract <- function(ISO, Level, dataset, timestep) {
   ## Merge data ----
   border_data <- dplyr::left_join(border_sf, mean_data, by = "ID")
   ## Export data to CSV ----
-  output_path <- file.path(main_path, paste0(ISO, dataset, "_", timestep, ".csv"))
+  output_path <- file.path(main_path, paste0(ISO, "_", ".csv"))
   write.csv(border_data, output_path, row.names = FALSE)
   return(border_data)
 }
-
-
 
 
